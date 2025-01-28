81cad7eaca19d7fcc80352b6dc7b6b3a6809d87f
Yash Hooda <67335307+yashhooda1@users.noreply.github.com>
2025-01-28 15:46:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('https://yashhooda1.github.io/yashhooda/#calculator', methods=['POST'])
+@app.route('/calculator', methods=['POST'])
 def calculate():
     try:
         goal = request.form['goal']

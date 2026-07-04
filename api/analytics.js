671818abd13e3dfb5ae744bed0b9377f0c54e3cc
Yash Hooda <67335307+yashhooda1@users.noreply.github.com>
2026-07-04 13:57:56 -0500

    remove bug/issue

--- api/analytics.js
+++ api/analytics.js
@@ -1,5 +1,3 @@
-import { getRunWeather } from "./lib/weatherEnrichment.js";
-
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');

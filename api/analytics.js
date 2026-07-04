f7a50e10504e245d4d10dfac925a237d32bba841
Yash Hooda <67335307+yashhooda1@users.noreply.github.com>
2026-07-04 13:30:36 -0500

    Add import for getRunWeather function
    Import getRunWeather function from weatherEnrichment.js.

--- api/analytics.js
+++ api/analytics.js
@@ -1,3 +1,5 @@
+import { getRunWeather } from "./lib/weatherEnrichment.js";
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
